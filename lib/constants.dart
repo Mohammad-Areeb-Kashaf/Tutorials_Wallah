2baeeb6035c2923865a5f0acbbd89abb9ce92239
AK Developer <codewithak786@gmail.com>
2022-10-07 10:08:27 +0530

    Authentication and their screens done

--- lib/constants.dart
+++ lib/constants.dart
@@ -1,22 +1,24 @@
 import 'package:flutter/material.dart';
 
-var kBackground = Container(
-  decoration: BoxDecoration(
-    gradient: LinearGradient(
-      begin: Alignment.topCenter,
-      end: Alignment.bottomCenter,
-      stops: [
-        0.1,
-        0.4,
-        0.7,
-        0.9,
-      ],
-      colors: [
-        Colors.deepPurple.shade600,
-        Colors.deepPurple.shade700,
-        Colors.deepPurple.shade800,
-        Colors.deepPurple.shade900,
-      ],
-    ),
+var kBackgroundDecoration = BoxDecoration(
+  gradient: LinearGradient(
+    begin: Alignment.topCenter,
+    end: Alignment.bottomCenter,
+    stops: [
+      0.1,
+      0.4,
+      0.7,
+      0.9,
+    ],
+    colors: [
+      Colors.deepPurple.shade600,
+      Colors.deepPurple.shade700,
+      Colors.deepPurple.shade800,
+      Colors.deepPurple.shade900,
+    ],
   ),
+);
+
+var kTextFieldHintStyle = TextStyle(
+  color: Colors.grey.shade400,
 );
