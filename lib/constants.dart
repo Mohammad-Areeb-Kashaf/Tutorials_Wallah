22c0f2957eb510cc0240af0de8c20bb1e12b1e42
AK Developer <codewithak786@gmail.com>
2022-10-20 22:53:24 +0530

    Fetching Playlists the right way

--- lib/constants.dart
+++ lib/constants.dart
@@ -1,20 +1,23 @@
 import 'package:flutter/material.dart';
 
-final kBackground = BoxDecoration(
-  gradient: LinearGradient(
-    begin: Alignment.topCenter,
-    end: Alignment.bottomCenter,
-    stops: [
-      0.1,
-      0.4,
-      0.7,
-      0.9,
-    ],
-    colors: [
-      Colors.deepPurple.shade600,
-      Colors.deepPurple.shade700,
-      Colors.deepPurple.shade700,
-      Colors.deepPurple.shade800,
-    ],
-  ),
-);
+class Constants {
+  static BoxDecoration kBackground = BoxDecoration(
+    gradient: LinearGradient(
+      begin: Alignment.topCenter,
+      end: Alignment.bottomCenter,
+      stops: [
+        0.1,
+        0.4,
+        0.7,
+        0.9,
+      ],
+      colors: [
+        Colors.deepPurple.shade600,
+        Colors.deepPurple.shade700,
+        Colors.deepPurple.shade700,
+        Colors.deepPurple.shade800,
+      ],
+    ),
+  );
+  static String API_KEY = 'AIzaSyAHUuUvdMplrWLILnQM8ilD7-GD4jJWEYs';
+}
