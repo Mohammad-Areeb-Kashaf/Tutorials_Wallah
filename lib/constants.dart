0fa2114612bae2e23d8435534fe88ca6a32b30ef
AK Developer <codewithak786@gmail.com>
2022-11-19 18:22:19 +0530

    Enhanced UI and UX

--- lib/constants.dart
+++ lib/constants.dart
@@ -1,11 +1,7 @@
 import 'package:flutter/material.dart';
 
 class Constants {
-  static BoxDecoration kBackground = const BoxDecoration(
-      gradient: LinearGradient(
-    colors: [Color(0xff3f51b5), Color(0xff6539b3)],
-    begin: Alignment.bottomCenter,
-    end: Alignment.topCenter,
-  ));
-  static String API_KEY = 'AIzaSyAHUuUvdMplrWLILnQM8ilD7-GD4jJWEYs';
+  static const purpleColor = Color(0xff7620E6);
+  static const BoxDecoration kBackground = BoxDecoration(color: Colors.white);
+  static const String API_KEY = 'AIzaSyAHUuUvdMplrWLILnQM8ilD7-GD4jJWEYs';
 }
